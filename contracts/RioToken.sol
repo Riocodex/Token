ab0451ac4dc208844ccfdf9401e572ade75b3372
Rio <onwukachibike@gmail.com>
2023-01-28 17:46:11 +0100

    created contract

---
+++ contracts/RioToken.sol
@@ -0,0 +1,12 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.17;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.8.1/contracts/token/ERC20/ERC20.sol";
+
+
+contract RIOToken is ERC20 {
+    constructor(uint256 initialSupply) ERC20("RIOTOKEN", "RIO") {
+        _mint(msg.sender, initialSupply);
+    }
+}
