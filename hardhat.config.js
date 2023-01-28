b9f24de9f482ea278e3e3b2787ec7c786e22f737
Rio <onwukachibike@gmail.com>
2023-01-28 22:11:19 +0100

    updated

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,5 +3,12 @@ require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.17"
+  solidity: "0.8.17",
+  networks: {
+    
+    goerli: {
+      url: process.env.INFURA_GOERLI_ENDPOINT,
+      accounts: [process.env.PRIVATE_KEY]
+    }
+  }
 };
