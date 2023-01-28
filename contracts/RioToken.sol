1fafc5c7934c7f998104614cd967c95e676dc129
Rio <onwukachibike@gmail.com>
2023-01-28 18:02:32 +0100

    updated

--- contracts/RioToken.sol
+++ contracts/RioToken.sol
@@ -3,9 +3,10 @@
 pragma solidity ^0.8.17;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
 
 
-contract RIOToken is ERC20 {
+contract RIOToken is ERC20Capped {
     address payable public owner;
     constructor(uint256 initialSupply) ERC20("RIOTOKEN", "RIO") {
         owner = msg.sender;
