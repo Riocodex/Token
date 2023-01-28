7196bb09850d08afd89adedee4aaa9457f6d3c72
Rio <onwukachibike@gmail.com>
2023-01-28 18:20:48 +0100

    created setBlock reward function

--- contracts/RioToken.sol
+++ contracts/RioToken.sol
@@ -5,9 +5,10 @@ pragma solidity ^0.8.17;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 
-contract RIOToken is ERC20Capped {
+contract RIOToken is ERC20Capped,Ownable {
     address payable public owner;
     uint256 public blockReward;
     constructor(uint256 cap, uint256 reward) ERC20("RIOTOKEN", "RIO") ERC20Capped(cap * (10 ** decimals())) {
@@ -15,4 +16,10 @@ contract RIOToken is ERC20Capped {
         _mint(owner, 70000000 * (10 ** decimals()));
         blockReward = reward * (10 ** decimals());
     }
+
+    function setBlockReward(uint256 reward)public onlyOwner{
+        blockReward = reward * (10 ** decimals());;
+    }
+
+
 }
