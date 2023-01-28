818a7d417448cbb4eb8e6672b559cfa0f61c5b4a
Rio <onwukachibike@gmail.com>
2023-01-28 22:21:31 +0100

    updated

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,1 +1,16 @@
 const hre = require("hardhat");
+
+async function main(){
+    const RioToken = await hre.ethers.getContractFactory("RioToken");
+    const rioToken = await RioToken.deploy(100000000, 50);
+
+    await rioToken.deployed();
+
+    console.log("Rio Token deployed @ ",rioToken.address);
+}
+
+
+main().catch((error) =>{
+    console.error(error);
+    process.exitCode=1;
+  });
