9f3323d5dff0b838af43490e0a08b616322f0bdf
Rio <onwukachibike@gmail.com>
2023-01-30 19:00:00 +0100

    added interface

--- contracts/Faucet.sol
+++ contracts/Faucet.sol
@@ -2,6 +2,11 @@
 
 pragma solidity ^0.8.17;
 
+interface IERC20 {
+    function transfer(address to, uint256 amount) external view returns(bool);
+    function balanceOf(address account) external view returns(uint256);
+}
+
 contract Faucet{
     address payable owner;
 }
