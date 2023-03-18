9322a833940a07ab23660d2fdca68f8146c93df4
Rio <onwukachibike@gmail.com>
2023-03-18 23:53:01 +0100

    updated

--- contracts/RioToken.sol
+++ contracts/RioToken.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.17;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
-contract RioToken is ERC20Capped, ERC20Burnable {
+contract RioToken is ERC20Burnable {
     address payable public owner;
     uint256 public blockReward;
 
