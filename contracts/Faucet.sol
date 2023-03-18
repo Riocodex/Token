341404a25c7701fcaeac083e39eb5964582937c5
Rio <onwukachibike@gmail.com>
2023-03-18 22:39:14 +0100

    :updated

--- contracts/Faucet.sol
+++ contracts/Faucet.sol
@@ -11,12 +11,14 @@ contract Faucet{
     address payable owner;
     IERC20 public token;
 
+    uint256 public withdrawalAmount = 50 * (10**18);
     constructor(address tokenAddress) payable{
         token = IERC20(tokenAddress);
         owner = payable(msg.sender); 
     }
 
     function requestTokens() public{
+        require(msg.sender != address(0), "Request must not originate from a zero Account");
         token.transfer(msg.sender, withdrawalAmount); 
     }
 }
