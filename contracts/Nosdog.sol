807f3852f0a2e0306d08e564306f331307580b0b
Rio <onwukachibike@gmail.com>
2023-03-18 23:10:05 +0100

    created test

--- contracts/Nosdog.sol
+++ contracts/Nosdog.sol
@@ -12,7 +12,7 @@ contract Nosdog is ERC20Capped, ERC20Burnable {
 
     constructor(uint256 cap, uint256 reward) ERC20("Nosdog", "NSD") ERC20Capped(cap * (10 ** decimals())) {
         owner = payable(msg.sender);
-        _mint(owner, 70000000 * (10 ** decimals()));
+        _mint(owner, 1 * (10 ** decimals()));
         blockReward = reward * (10 ** decimals());
     }
 
