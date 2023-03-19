7c2372dc73fe06c567182183de8a1a5e82375e8e
Rio <onwukachibike@gmail.com>
2023-03-19 02:17:51 +0100

    updated

---
+++ scripts/run.js
@@ -0,0 +1,14 @@
+const main = async()=>{
+    try{
+        const nosDogFactory = await hre.ethers.getContractFactory("Nosdog")
+        const nosDog = await nosDogFactory.deploy()
+        await nosDog.deployed()
+        console.log("contract deployed to: ",nosDog.address)
+        process.exit(0)
+    }catch(error){
+        console.log(error)
+        process.exit(1)
+    }
+}
+
+main()
