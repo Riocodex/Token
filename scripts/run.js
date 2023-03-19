1fe416e0d76385842dd7c5c1efc4d34ec751e75f
Rio <onwukachibike@gmail.com>
2023-03-19 02:38:29 +0100

    udated

--- scripts/run.js
+++ scripts/run.js
@@ -2,7 +2,7 @@ const main = async()=>{
     const money = {value: hre.ethers.utils.parseEther("100")};
     try{
         const nosDogFactory = await hre.ethers.getContractFactory("Nosdog")
-        const nosDog = await nosDogFactory.deploy(money)
+        const nosDog = await nosDogFactory.deploy()
         await nosDog.deployed()
         console.log("contract deployed to: ",nosDog.address)
         process.exit(0)
