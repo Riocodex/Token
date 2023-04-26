61014474ffceb4caf4ceab116bfea80ec64e5d7b
Rio <onwukachibike@gmail.com>
2023-04-27 00:23:14 +0100

    updated

--- contracts/Faucet.sol
+++ contracts/Faucet.sol
@@ -1,44 +1,57 @@
+// contracts/Faucet.sol
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.17;
 
 interface IERC20 {
-    function transfer(address to, uint256 amount) external view returns(bool);
-    function balanceOf(address account) external view returns(uint256);
+    function transfer(address to, uint256 amount) external returns (bool);
+
+    function balanceOf(address account) external view returns (uint256);
+
     event Transfer(address indexed from, address indexed to, uint256 value);
 }
 
-contract Faucet{
+contract Faucet {
     address payable owner;
     IERC20 public token;
 
     uint256 public withdrawalAmount = 50 * (10**18);
     uint256 public lockTime = 1 minutes;
 
-    event Withdrawl(address indexed to, uint256 indexed amount);
-    event Deposit(address from, uint256 amount);
+    event Withdrawal(address indexed to, uint256 indexed amount);
+    event Deposit(address indexed from, uint256 indexed amount);
 
     mapping(address => uint256) nextAccessTime;
-    constructor(address tokenAddress) payable{
+
+    constructor(address tokenAddress) payable {
         token = IERC20(tokenAddress);
-        owner = payable(msg.sender); 
+        owner = payable(msg.sender);
     }
 
-    function requestTokens() public{
-        //checking if the request is coming from an original account
-        require(msg.sender != address(0), "Request must not originate from a zero Account");
-        require(token.balanceOf(address(this)) >= withdrawalAmount, "Insufficient balance in faucet for withdrawal");
-        require(block.timestamp >= nextAccessTime[msg.sender],"insufficient time elapsed since last withdraw - try again later");
+    function requestTokens() public {
+        require(
+            msg.sender != address(0),
+            "Request must not originate from a zero account"
+        );
+        require(
+            token.balanceOf(address(this)) >= withdrawalAmount,
+            "Insufficient balance in faucet for withdrawal request"
+        );
+        require(
+            block.timestamp >= nextAccessTime[msg.sender],
+            "Insufficient time elapsed since last withdrawal - try again later."
+        );
 
-        nextAccessTime[msg.sender] = block.timestamp + lockTime; 
-        token.transfer(msg.sender, withdrawalAmount); 
+        nextAccessTime[msg.sender] = block.timestamp + lockTime;
+
+        token.transfer(msg.sender, withdrawalAmount);
     }
 
     receive() external payable {
         emit Deposit(msg.sender, msg.value);
     }
 
-    function getBalance() external view returns(uint256){
+    function getBalance() external view returns (uint256) {
         return token.balanceOf(address(this));
     }
 
@@ -50,13 +63,16 @@ contract Faucet{
         lockTime = amount * 1 minutes;
     }
 
-    function withdrawal() external onlyOwner{
-        emit Withdrawl(msg.sender, token.balanceOf(address(this)));
+    function withdraw() external onlyOwner {
+        emit Withdrawal(msg.sender, token.balanceOf(address(this)));
         token.transfer(msg.sender, token.balanceOf(address(this)));
     }
 
-    modifier onlyOnwer() {
-        require(msg.sender == owner, "only the contract owner can call this function");
+    modifier onlyOwner() {
+        require(
+            msg.sender == owner,
+            "Only the contract owner can call this function"
+        );
         _;
     }
 }
