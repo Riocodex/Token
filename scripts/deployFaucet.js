98f348022b78bfc938193cee95efbc98d92336de
Rio <onwukachibike@gmail.com>
2023-04-27 16:24:37 +0100

    updated

--- scripts/deployFaucet.js
+++ scripts/deployFaucet.js
@@ -1,17 +1,15 @@
 const hre = require("hardhat");
 
-async function main(){
-    const Faucet = await hre.ethers.getContractFactory("RioToken");
-    const faucet = await Faucet.deploy("0x90d61929B89441113fa06DC1B74Df2eAc877e9e9");
+async function main() {
+  const Faucet = await hre.ethers.getContractFactory("Faucet");
+  const faucet = await Faucet.deploy("0x90d61929B89441113fa06DC1B74Df2eAc877e9e9");
 
-    await faucet.deployed();
+  await faucet.deployed();
 
-    console.log("faucet contract deployed @ ",faucet.address);
-    //address: 0xAa62D1413022D26ee1a96ADdb6fBf843B86Ff1c3
+  console.log("Facuet contract deployed: ", faucet.address);
 }
 
-
-main().catch((error) =>{
-    console.error(error);
-    process.exitCode=1;
-  });
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
